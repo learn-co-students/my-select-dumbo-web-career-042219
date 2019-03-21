be583cf714981f3a185a73255ea85c8787f990eb
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-21 15:42:01 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  new_array = []
+  while i < collection.length
+  new_array<< (yield collection[i.include?()])
+    i = i + 1
+  end
+  new_array
 end
+
