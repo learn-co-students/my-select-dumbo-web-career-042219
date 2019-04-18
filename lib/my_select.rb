e88d79221fe49e1ab2403e2e6af7fa3252451ddc
Chloe <gingkolane@gmail.com>
2019-04-18 13:53:59 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,26 @@
+# require 'pry'
+
 def my_select(collection)
  # your code here!
+   if collection == []
+     "This block should not run!"
+
+
+# WHAT IS THIS?!
+   elseif if block_given? == false
+     collection
+
+   else
+       output = []
+       i = 0
+       while i < collection.length
+         result = yield collection[i]
+
+         if result == true
+           output << collection[i]
+         end
+         i = i + 1
+       end
+       output
+   end
 end
